8e0faaff12ecc3f6c8edf3a300004cb1e6c4c01d
Dmitry <dmi3z@yandex.ru>
2019-11-17 00:07:46 +0300

    port

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.port || 3000;
+const port = process.env.port;
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
